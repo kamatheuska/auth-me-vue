0eb88025ef5a6cdb5653dc8f99ab4771427c3a72
Nicolas Ramirez <nico9017@gmail.com>
2018-10-28 23:03:12 +0100

    Publishing to npm

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-/* 
-
 // Import vue component
 import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
+import server from './modules/server'
+import users from './modules/users'
 
 // Declare install function executed by Vue.use()
 export function install (Vue) {
     if (install.installed) return
     install.installed = true
-    Vue.component('MyComponent', component);
+    Vue.component('Login', Login)
+    Vue.component('Signup', Signup)
 }
 
 // Create module definition for Vue.use()
 const plugin = {
-    install,
-};
+    install
+}
 
 // Auto-install when vue is found (eg. in browser via <script> tag)
-let GlobalVue = null;
+let GlobalVue = null
 if (typeof window !== 'undefined') {
-    GlobalVue = window.Vue;
+    GlobalVue = window.Vue
 } else if (typeof global !== 'undefined') {
-    GlobalVue = global.Vue;
+    GlobalVue = global.Vue
 }
 if (GlobalVue) {
-    GlobalVue.use(plugin);
+    GlobalVue.use(plugin)
 }
 
 // To allow use as module (npm/webpack/etc.) export component
-export default component;
-
- */
+export { Login, Signup, users, server }
